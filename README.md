7af550e17f27e070ac1d35961af75f621893ce61
Bryan Chun <chun.hiu.sang@gmail.com>
2019-11-16 23:00:45 +0800

    changed function yml to be dynamic to ip and docker username, and added some more minio test code

--- README.md
+++ README.md
@@ -4,3 +4,7 @@
 
 1. Follow instructions in https://github.com/bryanchun/faas
 2. Run `. ./host.sh` to start local development
+3. Install npm packages by `cd m2h; npm install; cd ..` and `cd md2html; npm install; cd ..`
+4. To deploy a function: specify the Kubenetes ip as `$MINIKUBE_IP`, docker username as `$DOCKER_USERNAME`, and pass in the function configuration yml. For example:
+
+    `MINIKUBE_IP=$(minikube ip) DOCKER_USERNAME=bryanchun faas-cli up -f m2h.yml`
--- m2h/handler.js
+++ m2h/handler.js
@@ -1,9 +1,36 @@
 "use strict"
-const showdown = require('showdown')
+const Showdown = require('showdown')
+const Minio = require('minio')
 
 module.exports = (event, context) => {
-    var converter = new showdown.Converter(),
+    var converter = new Showdown.Converter(),
         html = converter.makeHtml(event.query.md)
+
+    var minioClient = new Minio.Client({
+            endPoint: 'localhost',
+            port: 30322,
+            useSSL: false,
+            accessKey: 'minio',
+            secretKey: 'minio123'
+        })
+    var file = './minio.md'
+    minioClient.makeBucket('europetrip', 'us-east-1', function(err) {
+        if (err) return console.log(err)
+        
+        console.log('Bucket created successfully in "us-east-1".')
+        
+        var metaData = {
+            'Content-Type': 'application/octet-stream',
+            'X-Amz-Meta-Testing': 1234,
+            'example': 5678
+        }
+        // Using fPutObject API upload your file to the bucket europetrip.
+        minioClient.fPutObject('m2h', 'minio.md', file, metaData, function(err, etag) {
+            if (err) return console.log(err)
+            console.log('File uploaded successfully.')
+        })
+    })
+
     context
         .headers({ "ContentType": "text/html" })
         .status(200)
