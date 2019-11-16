91676f6d7ddfcde86957c73ff3604663a3dbbe04
Bryan Chun <chun.hiu.sang@gmail.com>
2019-11-16 23:25:07 +0800

    fixed missing npm dependency

--- m2h/handler.js
+++ m2h/handler.js
@@ -6,30 +6,32 @@ module.exports = (event, context) => {
     var converter = new Showdown.Converter(),
         html = converter.makeHtml(event.query.md)
 
+    console.log('starting minio')
     var minioClient = new Minio.Client({
-            endPoint: 'localhost',
+            endPoint: '192.168.99.135',
             port: 30322,
             useSSL: false,
             accessKey: 'minio',
             secretKey: 'minio123'
         })
-    var file = './minio.md'
-    minioClient.makeBucket('europetrip', 'us-east-1', function(err) {
-        if (err) return console.log(err)
+
+    // var file = './minio.md'
+    // minioClient.makeBucket('europetrip', 'us-east-1', function(err) {
+    //     if (err) return console.log(err)
         
-        console.log('Bucket created successfully in "us-east-1".')
+    //     console.log('Bucket created successfully in "us-east-1".')
         
-        var metaData = {
-            'Content-Type': 'application/octet-stream',
-            'X-Amz-Meta-Testing': 1234,
-            'example': 5678
-        }
-        // Using fPutObject API upload your file to the bucket europetrip.
-        minioClient.fPutObject('m2h', 'minio.md', file, metaData, function(err, etag) {
-            if (err) return console.log(err)
-            console.log('File uploaded successfully.')
-        })
-    })
+    //     var metaData = {
+    //         'Content-Type': 'application/octet-stream',
+    //         'X-Amz-Meta-Testing': 1234,
+    //         'example': 5678
+    //     }
+    //     // Using fPutObject API upload your file to the bucket europetrip.
+    //     minioClient.fPutObject('m2h', 'minio.md', file, metaData, function(err, etag) {
+    //         if (err) return console.log(err)
+    //         console.log('File uploaded successfully.')
+    //     })
+    // })
 
     context
         .headers({ "ContentType": "text/html" })
