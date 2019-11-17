67b8522b1d0f90d4525b13d45d50236d61c8df64
Bryan Chun <chun.hiu.sang@gmail.com>
2019-11-17 19:31:39 +0800

    added code to upload plain string as file to Minio

--- m2h/handler.js
+++ m2h/handler.js
@@ -13,6 +13,10 @@ module.exports = (event, context) => {
     var converter = new Showdown.Converter(),
         html = converter.makeHtml(event.query.md)
 
+    /**
+     * Plain text file upload to Minio
+     * */
+
     var mc = new Minio.Client({
             endPoint: process.env.minio_endpoint,
             port: Number(process.env.minio_port),
@@ -20,29 +24,44 @@ module.exports = (event, context) => {
             accessKey: process.env.minio_accessKey,
             secretKey: process.env.minio_secretKey
         })
-
-
-    // var filename = "tmpfile"
-    // const file = fs.createWriteStream(filename)
-    // const request = http.get("http://images.pexels.com/photos/72161/pexels-photo-72161.jpeg?dl&fit=crop&w=640&h=318", (response) => {
-    //     response.pipe(file)
-    // })
+    const bucketName = "incoming"
     
-    // mc.makeBucket('incoming', 'us-east-1', function(err) {
-    //     if (err) return console.log(err)
-    //     console.log('Bucket created successfully in "us-east-1".')
-        
-    //     var metaData = {
-    //         'Content-Type': 'application/octet-stream',
-    //         'X-Amz-Meta-Testing': 1234,
-    //         'example': 5678
-    //     }
-    //     // Using fPutObject API upload your file to the bucket europetrip.
-    //     mc.fPutObject('incoming', 'image1', filename, metaData, function(err, etag) {
-    //         if (err) return console.log(err)
-    //         console.log('File uploaded successfully.')
-    //     })
-    // })
+    /* Create and write plain text file */
+    const filename = "tmp"
+    fs.writeFile(filename, "Hello Minio!", (err) => {
+        if (err) return console.err(err)
+        console.log("File written")
+    })
+    const fileStream = fs.createReadStream(filename)
+
+    /* Get a bucket, create it if it does not yet exist */
+    mc.bucketExists(bucketName, (err, exists) => {
+        if (err) return console.err(err)
+        if (!exists) {
+            mc.makeBucket(bucketName, 'us-east-1', (err) => {
+                if (err) { return console.log(err) }
+                console.log('Bucket created successfully in "us-east-1".')
+            })
+        }
+
+        /* Upload to Minio */
+        fs.stat(filename, (err, stats) => {
+            if (err) return console.log(err)
+            mc.putObject(bucketName, 'hellofile', fileStream, stats.size, (err, etag) => {
+                return console.log(err, etag)
+            })
+        })
+    })
+
+    /* List buckets */
+    mc.listBuckets((err, buckets) => {
+        if (err) return console.log(err)
+        console.log('buckets :', buckets)
+    })
+
+    /* Get file from bucket */
+    mc.fGetObject(bucketName, filename, "/tmp/" + filename)
+
 
     context
         .headers({ "Content-Type": "text/html" })
