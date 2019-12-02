e87ccc7865d965a35a583cdc99d5485f13411b82
Bryan Chun <chun.hiu.sang@gmail.com>
2019-12-03 00:35:59 +0800

    cleaned up file upload demo code and trying file upload

--- m2h/handler.js
+++ m2h/handler.js
@@ -1,34 +1,74 @@
 "use strict"
 
-const http = require('http')
 const fs = require('fs')
+const path = require('path')
+// const http = require('http')
 
 /**
  * NPM modules
  */
 const Showdown = require('showdown')
 const Minio = require('minio')
+const Formidable = require('formidable')
+const Express = require('express')
+
+const app = new Express()
+var mc = new Minio.Client({
+    endPoint: process.env.minio_endpoint,
+    port: Number(process.env.minio_port),
+    useSSL: false,
+    accessKey: process.env.minio_accessKey,
+    secretKey: process.env.minio_secretKey
+})
+const uploadDir = path.join(__dirname, '/uploads/')
 
 module.exports = (event, context) => {
+    /* Input file handling */
+    app.get('/', (req, res) => {
+        res.sendFile(path.join(__dirname, '/index.html'))
+    })
+    app.post('/upload', minioUploadHandler)
+    minioUploadDemo()
+
+    /* Output rendering */
     var converter = new Showdown.Converter(),
         html = converter.makeHtml(event.query.md)
+    context
+        .headers({ "Content-Type": "text/html" })
+        .status(200)
+        .succeed(html)
+}
+
+const minioUploadHandler = (req, res) => {
+    /* Local file upload */
+    var form = new Formidable.IncomingForm()
+    form.multiples = true
+    form.keepExtensions = true
+    form.uploadDir = uploadDir
+    form.parse(req, (err, fields, files) => {
+        if (err) return res.status(500).json({ error: err })
+        res.status(200).json({ uploaded: true })
+    })
+    form.on('fileBegin', function (name, file) {
+        const [fileName, fileExt] = file.name.split('.')
+        file.path = path.join(uploadDir, `${fileName}_${new Date().getTime()}.${fileExt}`)
+    })
+
+    /* Minio file upload from local */
+}
+
+const minioUploadDemo = (path) => {
+    const   region = "us-east-1",
+            bucketName = "incoming",
+            filename = "hellofile",
+            dummy_content = "Hello Minio!"
 
     /**
      * Plain text file upload to Minio
      * */
 
-    var mc = new Minio.Client({
-            endPoint: process.env.minio_endpoint,
-            port: Number(process.env.minio_port),
-            useSSL: false,
-            accessKey: process.env.minio_accessKey,
-            secretKey: process.env.minio_secretKey
-        })
-    const bucketName = "incoming"
-    
     /* Create and write plain text file */
-    const filename = "tmp"
-    fs.writeFile(filename, "Hello Minio!", (err) => {
+    fs.writeFile(filename, dummy_content, (err) => {
         if (err) return console.err(err)
         console.log("File written")
     })
@@ -38,16 +78,16 @@ module.exports = (event, context) => {
     mc.bucketExists(bucketName, (err, exists) => {
         if (err) return console.err(err)
         if (!exists) {
-            mc.makeBucket(bucketName, 'us-east-1', (err) => {
+            mc.makeBucket(bucketName, region, (err) => {
                 if (err) { return console.log(err) }
-                console.log('Bucket created successfully in "us-east-1".')
+                console.log(`Bucket created successfully in ${region}.`)
             })
         }
 
         /* Upload to Minio */
         fs.stat(filename, (err, stats) => {
             if (err) return console.log(err)
-            mc.putObject(bucketName, 'hellofile', fileStream, stats.size, (err, etag) => {
+            mc.putObject(bucketName, filename, fileStream, stats.size, (err, etag) => {
                 return console.log(err, etag)
             })
         })
@@ -61,10 +101,4 @@ module.exports = (event, context) => {
 
     /* Get file from bucket */
     mc.fGetObject(bucketName, filename, "/tmp/" + filename)
-
-
-    context
-        .headers({ "Content-Type": "text/html" })
-        .status(200)
-        .succeed(html)
 }
