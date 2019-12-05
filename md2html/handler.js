453cd8d726aaac23981c4dd0451106da5892e15e
Bryan Chun <chun.hiu.sang@gmail.com>
2019-12-05 23:55:21 +0800

    Added html landing page and solved 304 error on some machines

--- md2html/handler.js
+++ md2html/handler.js
@@ -1,17 +1,22 @@
 "use strict"
 
-module.exports = async (config) => {
-    const app = config.app;
+const path = require('path')
 
-    console.log('before a route');
+module.exports = async (config) => {
+    const app = config.app
+    // app.disable('etag')
+    app.use((req, res, next) => {
+      res.set('Cache-Control', 'no-store, no-cache, must-revalidate, private')
+      next()
+    })
 
     app.get('/',  (req, res) => {
-        console.log('in a route');
-        res.send('hello world');
-    });
+        console.log('in a route')
+        res.sendFile(path.join(__dirname, 'index.html'))
+        // res.send('root!')
+    })
 
     app.get('/*',  (req, res) => {
-      res.send('arbitrary');
-    });
-    console.log('at the end');
+      res.send('arbitrary')
+    })
 }
